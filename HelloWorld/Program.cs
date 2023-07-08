e24a7e47bccdcd5492e0f1127d6dff474600dc8c
bvanderzalm <70347711+bvanderzalm@users.noreply.github.com>
2023-07-08 17:35:59 -0400

    Different numerical types regarding memory

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -12,7 +12,36 @@ namespace HelloWorld // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(args[0]);
+            // Console.WriteLine(args[0]);
+
+            // Useful for storing constants more efficiently
+            sbyte maxValueSbyte = 127;
+            sbyte minValueSbyte = -128;
+            byte maxValueByte = 255;
+            byte minValueByte = 0;
+
+            short minValueShort = -32768;
+            ushort maxValueUshort = 65535;
+
+            int maxValueInt = 2147483647;
+            int minValueInt = -2147483648;
+
+            long maxLong = 9223372036854775807;
+            long minLong = -9223372036854775808;
+
+            float myFloat = 0.751f;
+            double myDouble = 0.75d;
+
+            decimal myDecimal = 0.751m;
+            float mySecondFloat = 0.75f;
+            double mySecondDouble = 0.751d;
+
+            decimal mySecondDecimal = 0.75m;
+
+            Console.WriteLine(myFloat - mySecondFloat);
+            Console.WriteLine(myDouble - mySecondDouble);
+            Console.WriteLine(myDecimal - mySecondDecimal);
+
         }
     }
 }
