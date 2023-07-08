51305023387c02afe6ba66d6ab22861647599cde
bvanderzalm <70347711+bvanderzalm@users.noreply.github.com>
2023-07-08 17:23:11 -0400

    Old fomatting

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,5 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
-Console.Write("First");
-Console.Write("Second"); // println() vs print() in java
+// Console.Write("First");
+// Console.Write("Second"); // println() vs print() in java
+
+using System;
+
+namespace HelloWorld // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+        }
+    }
+}
