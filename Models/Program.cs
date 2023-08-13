323c3ae328b6ebe5557e96cd9e2f65a6e1d488f5
bvanderzalm <70347711+bvanderzalm@users.noreply.github.com>
2023-08-13 14:11:33 -0400

    lecture 26

---
+++ Models/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Models
+{
+    public class Computer
+    {
+        // private string _motherboard; // automatically set when we add get set
+        public string Motherboard { get; set; }
+        public int CPUCores { get; set; }
+        public bool HasWifi { get; set; }
+        public bool HasLte { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+        public string VideoCard { get; set; }
+
+        public Computer()
+        {
+            if (VideoCard == null)
+                VideoCard = "";
+            if (Motherboard == null)
+                VideoCard = "";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer myComputer = new Computer()
+            {
+                Motherboard = "Z690",
+                HasWifi = true,
+                HasLte = false,
+                ReleaseDate = DateTime.Now,
+                Price = 943.87m,
+                VideoCard = "RTX 2060",
+            };
+            myComputer.HasWifi = false;
+            Console.WriteLine(myComputer.Motherboard);
+            Console.WriteLine(myComputer.HasWifi);
+            Console.WriteLine(myComputer.HasLte);
+            Console.WriteLine(myComputer.ReleaseDate);
+            Console.WriteLine(myComputer.Price);
+            Console.WriteLine(myComputer.VideoCard);
+        }
+    }
+}
