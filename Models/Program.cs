ed1dcc431d0738a453ab1a0a657f23aaddedf66d
bvanderzalm <70347711+bvanderzalm@users.noreply.github.com>
2023-08-13 14:12:58 -0400

    Constructor improvement

--- Models/Program.cs
+++ Models/Program.cs
@@ -3,21 +3,13 @@
     public class Computer
     {
         // private string _motherboard; // automatically set when we add get set
-        public string Motherboard { get; set; }
+        public string Motherboard { get; set; } = "";
         public int CPUCores { get; set; }
         public bool HasWifi { get; set; }
         public bool HasLte { get; set; }
         public DateTime ReleaseDate { get; set; }
         public decimal Price { get; set; }
-        public string VideoCard { get; set; }
-
-        public Computer()
-        {
-            if (VideoCard == null)
-                VideoCard = "";
-            if (Motherboard == null)
-                VideoCard = "";
-        }
+        public string VideoCard { get; set; } = "";
     }
 
     internal class Program
